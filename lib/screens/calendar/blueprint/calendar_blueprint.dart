fa0c081fdab73f2ebd59578f3620f2c7cec58765
CarlosGonzalez0211 <116935841+CarlosGonzalez0211@users.noreply.github.com>
2024-04-27 21:53:54 -0600

    Calendar Implementation

--- lib/screens/calendar/blueprint/calendar_blueprint.dart
+++ lib/screens/calendar/blueprint/calendar_blueprint.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
+import '../modules/event.dart';
+
 class CalendarBlueprint extends StatefulWidget {
   const CalendarBlueprint({super.key});
 
@@ -9,41 +11,152 @@ class CalendarBlueprint extends StatefulWidget {
 }
 
 class _CalendarBlueprintState extends State<CalendarBlueprint> {
+  late Map<DateTime, List<Event>> selectedEvents;
+  CalendarFormat format = CalendarFormat.month;
+  DateTime selectedDay = DateTime.now();
+  DateTime focusedDay = DateTime.now();
 
-  DateTime today  = DateTime.now();
-  void _onDaySelected(DateTime day, DateTime focusedDay){
-    setState(() {
-      today = day;
-    });
+  final TextEditingController _eventController = TextEditingController();
+
+  @override
+  void initState() {
+    selectedEvents = {};
+    super.initState();
+  }
+
+  List<Event> _getEventsfromDay(DateTime date) {
+    return selectedEvents[date] ?? [];
+  }
+
+  @override
+  void dispose() {
+    _eventController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: content()
-    );
-  }
+      appBar: AppBar(
+        title: const Text("ESTech Calendar"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          TableCalendar(
+            focusedDay: selectedDay,
+            firstDay: DateTime(1990),
+            lastDay: DateTime(2050),
+            calendarFormat: format,
+            onFormatChanged: (CalendarFormat _format) {
+              setState(() {
+                format = _format;
+              });
+            },
+            startingDayOfWeek: StartingDayOfWeek.sunday,
+            daysOfWeekVisible: true,
 
-  Widget content() {
-    return Padding(
-      padding: const EdgeInsets.all(50.0),
-        child: Column(
-          children: [
-            Text("Selected day: ${today.toString().split(" ")[0]}"),
-            TableCalendar(
-              locale: "en_US",
-              rowHeight: 43,
-              headerStyle:
-                const HeaderStyle(formatButtonVisible:false, titleCentered: true),
-              availableGestures: AvailableGestures.all,
-              selectedDayPredicate: (day) => isSameDay(day, today),
-              focusedDay: today,
-              firstDay: DateTime.utc(2010, 10, 20),
-              lastDay: DateTime.utc(2030, 10, 20),
-              onDaySelected: _onDaySelected,
-            )
-          ],
-        )
+            //Day Changed
+            onDaySelected: (DateTime selectDay, DateTime focusDay) {
+              setState(() {
+                selectedDay = selectDay;
+                focusedDay = focusDay;
+              });
+              print(focusedDay);
+            },
+            selectedDayPredicate: (DateTime date) {
+              return isSameDay(selectedDay, date);
+            },
+
+            eventLoader: _getEventsfromDay,
+
+            //To style the Calendar
+            calendarStyle: CalendarStyle(
+              isTodayHighlighted: true,
+              selectedDecoration: BoxDecoration(
+                color: Colors.blue[100],
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              selectedTextStyle: const TextStyle(color: Colors.white),
+              todayDecoration: BoxDecoration(
+                color: Colors.purpleAccent[100],
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              defaultDecoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              weekendDecoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+            ),
+            headerStyle: HeaderStyle(
+              formatButtonVisible: true,
+              titleCentered: true,
+              formatButtonShowsNext: false,
+              formatButtonDecoration: BoxDecoration(
+                color: Colors.blue[100],
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              formatButtonTextStyle: const TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          ..._getEventsfromDay(selectedDay).map(
+                (Event event) => ListTile(
+              title: Text(
+                event.title,
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text("Add Event"),
+            content: TextFormField(
+              controller: _eventController,
+            ),
+            actions: [
+              TextButton(
+                child: const Text("Cancel"),
+                onPressed: () => Navigator.pop(context),
+              ),
+              TextButton(
+                child: const Text("Ok"),
+                onPressed: () {
+                  if (_eventController.text.isEmpty) {
+
+                  } else {
+                    if (selectedEvents[selectedDay] != null) {
+                      selectedEvents[selectedDay]?.add(
+                        Event(title: _eventController.text),
+                      );
+                    } else {
+                      selectedEvents[selectedDay] = [
+                        Event(title: _eventController.text)
+                      ];
+                    }
+
+                  }
+                  Navigator.pop(context);
+                  _eventController.clear();
+                  setState((){});
+                  return;
+                },
+              ),
+            ],
+          ),
+        ),
+        label: const Text("Add Event"),
+        icon: const Icon(Icons.add),
+      ),
     );
   }
 }
